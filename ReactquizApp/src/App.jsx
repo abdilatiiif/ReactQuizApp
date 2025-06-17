4c79e8e18ab24f900fd6454243b0827fb9af16d8
Latif <latif_1995@live.no>
2025-06-17 16:20:00 +0200

    fake lokal api

--- ReactquizApp/src/App.jsx
+++ ReactquizApp/src/App.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header.jsx";
 import MainContent from "./mainContent.jsx";
 
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log("state", state);
+  console.log("action", action);
+
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
